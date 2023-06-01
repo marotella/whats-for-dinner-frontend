407df0c89200f0f17306ca4bb835d6f9aab0bf29
marotella <michelle.a.rotella@gmail.com>
2023-06-01 19:14:09 -0400

    got ingredients to render homepage formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Home({loginUser, registerUser}) {
+function Home({ loginUser, registerUser }) {
     return (
         <div className="homeContainer">
             <div className="homeSection1">
@@ -13,15 +13,20 @@ function Home({loginUser, registerUser}) {
                     </div>
                 </div>
                 <div className="homeImage">
-                <img src={process.env.PUBLIC_URL + '/Veggies.jpg'} alt="vegetables" className="homeImage1"/>
+                    <img src={process.env.PUBLIC_URL + '/Veggies.jpg'} alt="vegetables" className="homeImage1" />
                 </div>
+            </div>
+            <div className="recipeCarousel">
+
             </div>
             <div className="homeSection2">
                 <div className="homeImage">
-
+                <img src={process.env.PUBLIC_URL + '/WFDingredients.jpg'} alt="ingredietns" className="homeImage1" />
                 </div>
                 <div className="description2">
-
+                    Store and update the ingredients you have on hand in <b>My Kitchen!</b> 
+                    <br/>
+                    Using the What's for Dinner recipe book, search for new and easy recipes that use what you have on hand and enjoy!
                 </div>
             </div>
         </div>
--- src/pages/RecipeInfo.js
+++ src/pages/RecipeInfo.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 const RecipeDetails = () => {
-    const {id} = useParams()
+    const { id } = useParams()
     const [recipeInfo, setRecipeInfo] = useState(null)
     useEffect(() => {
         fetch(`http://localhost:8000/ingredients/api/recipes/${id}`)
             .then((response) => response.json())
             .then((data) => {
                 setRecipeInfo(data)
-                console.log(recipeInfo)
+                console.log(data);
             })
             .catch((error) => {
                 console.error(error)
                 setRecipeInfo(null)
             })
 
+
     }, [id])
+
+    useEffect(() => {
+        console.log(recipeInfo);
+    }, [recipeInfo]);
+
     if (!recipeInfo) {
         return <h2>Flipping through the pages of the cookbook...</h2>
     }
@@ -24,7 +30,15 @@ const RecipeDetails = () => {
         <div>
             <h1>{recipeInfo.name}</h1>
             <p>Category: {recipeInfo.category}</p>
-            <img src={recipeInfo.thumbnail} alt= "food image" />
+            <img src={recipeInfo.thumbnail} alt="food image" />
+            <h2>Ingredients</h2>
+            <ul>
+                {recipeInfo.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient.ingredient} - {ingredient.measure}</li>
+                )
+                )
+                }
+            </ul>
             <h2>Instructions:</h2>
             <p>{recipeInfo.instructions}</p>
             <Link to="/ingredients/recipes" className="backLink"> Back </Link>

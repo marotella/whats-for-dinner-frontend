a6f8c2032c5cdbd65d41a2950db70eb35942da3a
marotella <michelle.a.rotella@gmail.com>
2023-06-03 12:54:27 -0400

    Tailwind on RecipeInfo

--- src/pages/RecipeInfo.js
+++ src/pages/RecipeInfo.js
@@ -23,25 +23,35 @@ const RecipeDetails = () => {
     }, [recipeInfo]);
 
     if (!recipeInfo) {
-        return <h2>Flipping through the pages of the cookbook...</h2>
+        return <h2>Flipping through the cookbook pages...</h2>
     }
 
     return (
         <div>
-            <h1>{recipeInfo.name}</h1>
-            <p>Category: {recipeInfo.category}</p>
-            <img src={recipeInfo.thumbnail} alt="food image" />
-            <h2>Ingredients</h2>
-            <ul>
-                {recipeInfo.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient.ingredient} - {ingredient.measure}</li>
-                )
-                )
-                }
-            </ul>
-            <h2>Instructions:</h2>
-            <p>{recipeInfo.instructions}</p>
-            <Link to="/ingredients/recipes" className="backLink"> Back </Link>
+            <h1 class="p-5 max-w-sm mx-auto text-center font-medium font-size text-3xl">{recipeInfo.name}</h1>
+            <p class="p-2 text-center">Category: {recipeInfo.category}</p>
+            <div class="flex row">
+                <div class="w-1/2 p-5">
+                    <h2 class="p-5 flex items-center font-medium max-w-sm mx-auto text-xl">Ingredients</h2>
+                    <ul>
+                        {recipeInfo.ingredients.map((ingredient, index) => (
+                            <li key={index}>{ingredient.ingredient} - {ingredient.measure}</li>
+                        )
+                        )
+                        }
+                    </ul>
+                </div>
+                <div class="w-1/2 p-5">
+                    <img class="w-full h-auto" src={recipeInfo.thumbnail} alt="food image" />
+                </div>
+            </div>
+            <div class="m-5">
+                <h2 class="font-medium text-xl">Instructions:</h2>
+                <p class="p-5 text-xs">1. {recipeInfo.instructions}</p>
+                <Link to="/ingredients/recipes" className="backLink"> Back </Link>
+            </div>
+
+
         </div>
     );
 };

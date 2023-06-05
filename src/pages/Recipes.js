c02086e31b6fec09f93cfd34eca73f0c06488c4b
marotella <michelle.a.rotella@gmail.com>
2023-06-05 17:51:22 -0400

    Search page formatting completed

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,7 +6,6 @@ const RecipeSearchForm = ({ ingredients, searchRecipes }) => {
     const [recipes, setRecipes] = useState([]);
     const [searchError, setSearchError] = useState(false);
 
-
     const handleIngredientChange = (event) => {
         const { value, checked } = event.target;
         if (checked) {
@@ -17,6 +16,7 @@ const RecipeSearchForm = ({ ingredients, searchRecipes }) => {
             );
         }
     };
+
     const handleRecipeSubmit = (event) => {
         event.preventDefault();
         fetch('http://localhost:8000/ingredients/api/search', {
@@ -28,82 +28,95 @@ const RecipeSearchForm = ({ ingredients, searchRecipes }) => {
         })
             .then((response) => response.json())
             .then((data) => {
-                // Handle the response data
                 console.log(data);
                 setRecipes(data.meals);
                 setRecipes(data.meals === null ? [] : data.meals);
                 console.log(recipes)
             })
             .catch((error) => {
-                // Handle any errors
                 console.error(error);
                 setRecipes([])
             });
     }
 
-
     const loaded = () => {
         console.log(ingredients);
         return (
-            <div class="flex text-center flex-col p-5 m-5">
-                <h3 class='text-xl font-medium p-2'>What's for Dinner Recipe Book</h3>
-
+            <div className="flex text-center flex-col p-5 m-5">
+                <h3 className='text-xl font-medium p-2'>
+                    What's for Dinner Recipe Book
+                </h3>
+                <p>
+                                Use your kitchen to discover new recipes!
+                                <br />
+                                <br />
+                                Select 1-3 items to search our cookbooks and discover something new to make for dinner.
+                            </p>
                 <div className="flex flex-wrap">
-                    <form onSubmit={handleRecipeSubmit}>
-                        <div class="flex flex-row">
-                            <p>Use your kitchen to discover new recipes!<br /><br /> Select 1-3 items to search our cookbooks and discover something new to make for dinner.</p>
-                            <button type="submit"><span class="material-symbols-outlined text-white bg-green text-4xl items-center">
-                                search
-                            </span>Search</button>
+                    <form onSubmit={handleRecipeSubmit} className="flex flex-col md:flex-row">
+                        <div className="w-full md:w-1/2">
+                        
+                            <div className="flex justify-center">
+                                <button type="submit" className="flex items-center bg-green text-white py-2 px-4 mt-4">
+                                    <span className="material-symbols-outlined text-4xl mr-2">
+                                        search
+                                    </span>
+                                    Search
+                                </button>
+                            </div>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
+                                {ingredients.map((ingredient) => (
+                                    <label className="ingredientSelector flex items-center flex-col border border-lightGreen border-2 bg-white" key={ingredient.ingredient}>
+                                        <div className="flex flex-row text-center justify-center items-center p-2">
+                                            <input
+                                                className="check m-1 p-2"
+                                                type="checkbox"
+                                                value={ingredient.ingredient}
+                                                checked={selectedIngredients.includes(ingredient.ingredient)}
+                                                onChange={handleIngredientChange}
+                                            />
+                                            <p class="p-4">{ingredient.ingredient}</p>
+                                        </div>
+                                        <div className="ingredientSelection flex items-center">
+                                            <img
+                                                className="ingredientThumb"
+                                                src={`https://www.themealdb.com/images/ingredients/${ingredient.ingredient}.png`}
+                                                alt={ingredient.ingredient}
+                                                onError={(e) => {
+                                                    e.target.src = process.env.PUBLIC_URL + '/Fork.png';
+                                                }}
+                                            />
+                                        </div>
+                                    </label>
+                                ))}
+                            </div>
                         </div>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 w-1/2"> {ingredients.map((ingredient) => (
-                            <label className="ingredientSelector flex items-center flex-col border border-lightGreen border-2">
-                                <div class="flex flex-row text-center justify-center items-center p-2">
-                                    <input
-                                        className="check m-1 p-2"
-                                        type="checkbox"
-                                        value={ingredient.ingredient}
-                                        checked={selectedIngredients.includes(ingredient.ingredient)}
-                                        onChange={handleIngredientChange}
-                                    />
-                                    <p class="p-4">{ingredient.ingredient}</p>
-                                </div>
-                                <div className="ingredientSelection flex items-center">
-                                    <img
-                                        className="ingredientThumb"
-                                        src={`https://www.themealdb.com/images/ingredients/${ingredient.ingredient}.png`}
-                                        alt={ingredient.ingredient}
-                                        onError={(e) => {
-                                            e.target.src = process.env.PUBLIC_URL + '/Fork.png';
-                                        }}
-                                    />
-                                </div>
-                            </label>
-                        ))}
-                        </div>
-                        <div className="results">
-                            {recipes && (recipes.length === 0 || recipes === null) ? (
-                                <h3>
-                                    Select a different ingredient or add to your kitchen
-                                </h3>
+                        <div className="w-full md:w-1/2 items-center p-2">
+                            <h3 className="text-center text-xl p-1">Search Results</h3>
+                            <div className="flex flex-col items-center justify-center">
+                                {recipes && (recipes.length === 0 || recipes === null) ? (
+                                    <h3>
+                                        Select an ingredient to search!
+                                    </h3>
 
-                            ) : (
-                                recipes.map((recipe) => (
-                                    <div className="recipeIcon w-1/2 flex flex-col  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2" key={recipe.idMeal}>
-                                        <Link to={`/ingredients/recipe/${recipe.idMeal}`} className="link"
-                                        ><h3>{recipe.strMeal} </h3></Link>
-                                        <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipeImage" />
-
-                                    </div>
-
-
-                                ))
-
-                            )}
+                                ) : (
+                                    recipes.map((recipe) => (
+                                        <div className="recipeIcon flex flex-col items-center border border-lightGreen border-2 bg-green text-white" key={recipe.idMeal}>
+                                            <h3 className="text-l font-medium">Recipe Card</h3>
+                                           <div className="flex flex-row justify-center items-center">
+                                            <Link to={`/ingredients/recipe/${recipe.idMeal}`} className="link"
+                                            ><h2 className="text-white hover:text-orange hover:underline text-center">{recipe.strMeal} </h2>
+                                            </Link>
+                                            <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipeImage m-3 items-center border-white border-2" />
+                                            </div>
+                                        </div>
+                                    ))
+                                )}
+                            </div>
                         </div>
                     </form >
-                </div>
-            </div>
+                </div >
+            </div >
         )
     };
 

a114a52be56a0144b294c03293d0337af3b5b46b
marotella <michelle.a.rotella@gmail.com>
2023-06-05 16:36:18 -0400

    Forms formatting fixed

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -47,17 +47,18 @@ const RecipeSearchForm = ({ ingredients, searchRecipes }) => {
         return (
             <div class="flex text-center flex-col p-5 m-5">
                 <h3 class='text-xl font-medium p-2'>What's for Dinner Recipe Book</h3>
-                <div class="flex text-left">
-                    <p>Use your kitchen to discover new recipes!<br /><br /> Select 1-3 items to search our cookbooks and discover something new to make for dinner.</p>
-                </div>
-                <div>
+
+                <div className="flex flex-wrap">
                     <form onSubmit={handleRecipeSubmit}>
-                        <button type="submit"><span class="material-symbols-outlined text-white bg-green text-4xl items-center">
-                            search
-                        </span>Search</button>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 w-3/5"> {ingredients.map((ingredient) => (
-                                <label className="ingredientSelector flex items-center flex-col border border-lightGreen border-2">
-                                    <div class="flex flex-row text-center justify-center items-center p-2">
+                        <div class="flex flex-row">
+                            <p>Use your kitchen to discover new recipes!<br /><br /> Select 1-3 items to search our cookbooks and discover something new to make for dinner.</p>
+                            <button type="submit"><span class="material-symbols-outlined text-white bg-green text-4xl items-center">
+                                search
+                            </span>Search</button>
+                        </div>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 w-1/2"> {ingredients.map((ingredient) => (
+                            <label className="ingredientSelector flex items-center flex-col border border-lightGreen border-2">
+                                <div class="flex flex-row text-center justify-center items-center p-2">
                                     <input
                                         className="check m-1 p-2"
                                         type="checkbox"
@@ -66,38 +67,38 @@ const RecipeSearchForm = ({ ingredients, searchRecipes }) => {
                                         onChange={handleIngredientChange}
                                     />
                                     <p class="p-4">{ingredient.ingredient}</p>
-                                    </div>
-                                    <div className="ingredientSelection flex items-center">
-                                        <img
-                                            className="ingredientThumb"
-                                            src={`https://www.themealdb.com/images/ingredients/${ingredient.ingredient}.png`}
-                                            alt={ingredient.ingredient}
-                                            onError={(e) => {
-                                                e.target.src = process.env.PUBLIC_URL + '/Fork.png';
-                                            }}
-                                        />
-                                    </div>
-                                </label>
+                                </div>
+                                <div className="ingredientSelection flex items-center">
+                                    <img
+                                        className="ingredientThumb"
+                                        src={`https://www.themealdb.com/images/ingredients/${ingredient.ingredient}.png`}
+                                        alt={ingredient.ingredient}
+                                        onError={(e) => {
+                                            e.target.src = process.env.PUBLIC_URL + '/Fork.png';
+                                        }}
+                                    />
+                                </div>
+                            </label>
                         ))}
                         </div>
-                        <div className="">
+                        <div className="results">
                             {recipes && (recipes.length === 0 || recipes === null) ? (
                                 <h3>
                                     Select a different ingredient or add to your kitchen
                                 </h3>
-                            
+
                             ) : (
                                 recipes.map((recipe) => (
-                                    <div className="recipeIcon" key={recipe.idMeal}>
+                                    <div className="recipeIcon w-1/2 flex flex-col  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2" key={recipe.idMeal}>
                                         <Link to={`/ingredients/recipe/${recipe.idMeal}`} className="link"
                                         ><h3>{recipe.strMeal} </h3></Link>
                                         <img src={recipe.strMealThumb} alt={recipe.strMeal} className="recipeImage" />
 
                                     </div>
-                                
+
 
                                 ))
-                                
+
                             )}
                         </div>
                     </form >
